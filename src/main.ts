bbe676520604997d5c3c6b07f83bb80c164ddfc9
PhatPhamFe <phatphamm@usumsoftware.com>
2022-05-16 23:06:51 +0700

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { resolve } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 3000;
-  await app.listen(PORT);
-  console.log('http://localhost:' + PORT);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  app.enableCors(options);
+  app.setBaseViewsDir(resolve('./public'));
+  app.setViewEngine('hbs');
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
